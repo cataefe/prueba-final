fc4daff70bf2c6f1df80be3a1dd4803ab8e424e3
Fernanda <fernandacatalina.pizarro@gmail.com>
2021-08-20 00:08:55 -0400

    change HTML CSS JS

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    $("a").click(function(event){
+    if(this.hasg !==""){
+    event.preventDefault(); 
+    
+    var gato = this.hash;
+
+    $("html, body").animate({
+        scrollTop: $(gato) .offset().top        
+        }, 800, function(){
+        window.location.hash = gato;
+    });    
+    }
+    });
+});
